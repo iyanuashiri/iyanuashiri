0ef99837ae684dfba78c9318d1dbdb8315cda652
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2020-09-28 21:39:44 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,37 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import VueScrollTo from 'vue-scrollto'
+import VueFuse from 'vue-fuse'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  Vue.use(VueScrollTo, {
+    duration: 500,
+    easing: "ease",
+  })
+
+  Vue.use(VueFuse)
+
+  head.meta.push({
+    name: 'keywords',
+    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+  })
+
+  head.meta.push({
+    name: 'description',
+    content: 'Iyanuoluwa Ajao'
+  })
+
+  head.meta.push({
+    name: 'author',
+    content: 'Iyanuoluwa Ajao'
+  })
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700'
+  })
 }
