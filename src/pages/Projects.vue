20910ceb1a84923729babdc4e093accdf2450561
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2020-12-02 15:58:33 +0100

    Update projects

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -3,7 +3,7 @@
     <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
         <div class="overflow-x-hidden">
           <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-            <h2 class="font-bold mb-6" id="projects">Here are some projects I've worked on:</h2>
+            <h2 class="font-bold mb-6" id="projects">Here are open source applications I have worked on:</h2>
 
             <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
               <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
@@ -11,47 +11,49 @@
 
             <ul class="text-lg sm:text-xl space-y-6">
               <li class="checkmark">
+                <div><a href="https://github.com/iyanuashiri/chaperone">chaperone</a></div>
+                <div class="text-lg text-gray-600">A browser extension to reduce social media usage.</div>
+              </li>
+              <li class="checkmark">
+                <div><a href="https://github.com/iyanuashiri/summarizerbot">summarizerbot</a></div>
+                <div class="text-lg text-gray-600">An AI Twitter bot to summarize any type of information.</div>
+              </li>
+              <li class="checkmark">
                 <div><a href="https://github.com/iyanuashiri/meethub">meethub</a></div>
                 <div class="text-lg text-gray-600">This is a Python/Django based event management system. A meetup clone.</div>
               </li>
               <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/stock-manager">stock-manager</a></div>
-                <div class="text-lg text-gray-600"></div>
-              </li>
-              <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/stock-manager-vue">stock-manager-vue</a></div>
-                <div class="text-lg text-gray-600"></div>
-              </li>
-              <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/twitterivity">twitterivity</a></div>
-                <div class="text-lg text-gray-600">A browser extension that improves your productivity by reducing your Twitter usage</div>
-              </li>
-              <li class="checkmark">
                 <div><a href="https://github.com/iyanuashiri/twilio-sms-action">twilio-sms-action</a></div>
-                <div class="text-lg text-gray-600">Twilio sms action</div>
+                <div class="text-lg text-gray-600">A GitHub action for Twilio SMS.</div>
               </li>
               <li class="checkmark">
                 <div><a href="https://github.com/iyanuashiri/celeroku">celeroku</a></div>
-                <div class="text-lg text-gray-600">Deploy Celery Flower to Heroku </div>
-              </li>
-              <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/wikipedia-reference-scraper">Wikipedia-reference-scraper</a></div>
-                <div class="text-lg text-gray-600">Wikipedia API wrapper for references </div>
-              </li>
-              <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/store">store</a></div>
-                <div class="text-lg text-gray-600">A tool to store terminal commands and create a backup on Google drive.</div>
-              </li>
-              <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/dj-cart">dj-cart</a></div>
-                <div class="text-lg text-gray-600">A simple shopping cart app for Django that is not session based</div>
-              </li>
-              <li class="checkmark">
-                <div><a href="https://github.com/iyanuashiri/africastalking_USSD_demo">africastalking_USSD_demo</a></div>
-                <div class="text-lg text-gray-600">Africastalking USSD Demo</div>
+                <div class="text-lg text-gray-600">monitor Celery processes on Heroku.</div>
               </li>
 
             </ul>
+
+            <h2 class="font-bold mb-6" id="libraries">Here are open source libraries I have worked on:</h2>
+
+            <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+              <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
+            </div>
+
+            <ul class="text-lg sm:text-xl space-y-6">
+              <li class="checkmark">
+                <div><a href="https://github.com/iyanuashiri/twitter-threader">twitter-threader</a></div>
+                <div class="text-lg text-gray-600"> A Twitter API library for threads.</div>
+              </li>
+              <li class="checkmark">
+                <div><a href="https://github.com/iyanuashiri/wikipedia-reference-scraper">Wikipedia-reference-scraper</a></div>
+                <div class="text-lg text-gray-600">A Wikipedia API wrapper for references.</div>
+              </li>
+              <li class="checkmark">
+                <div><a href="https://github.com/iyanuashiri/dj-cart">dj-cart</a></div>
+                <div class="text-lg text-gray-600">A simple shopping cart app for Django that is not session based.</div>
+              </li>
+            </ul>
+
           </div> <!-- end projects -->
         </div>
     </div>
