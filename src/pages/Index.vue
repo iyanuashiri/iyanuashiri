6af40b295a99eb4af1a56361963e476633fa8e46
Iyanuoluwa Ajao <ajaoiyanu@gmail.com>
2021-01-01 18:30:36 +0100

    add margin to my experience card

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,7 @@
       </div>
     </div> <!-- end hero -->
 
-    <div class="container-inner mx-auto">
+    <div class="container-inner mx-auto my-auto">
       <p class="text-lg sm:text-xl">I am a Software Engineer with about 5 years of experience noticing problems, building solutions, leveraging with technology
         and scaling solutions. I prefer Python for backend development, JavaScript for frontend and browser extension development, Google Cloud Platform; Heroku;
         and Netlify for cloud computing, and I have experience working with Flutter and Dart. I am currently learning Golang for commandline tools and Machine Learning. I know how to know things.
